be4167c0559d125add033e495c59c426fa1ebc50
Emil Stanchev <emils@fmi.uni-sofia.bg>
2010-05-18 02:06:02 +0300

    Triggers for numEnrolled and AssignmentNotification

--- createTables.sql
+++ createTables.sql
@@ -71,13 +71,14 @@ CREATE  TABLE  Category (
 -- -----------------------------------------------------
 -- Table Course
 -- -----------------------------------------------------
+
 CREATE  TABLE  Course (
   name VARCHAR(255) NOT NULL ,
   year SMALLINT NOT NULL ,
   Category_name VARCHAR(45) ,
   password VARCHAR(45),
   numEnrolled INT NOT NULL DEFAULT 0 ,
-  TeacherProfile_User_email VARCHAR(255) NOT NULL ,
+  TeacherProfile_User_email VARCHAR(255),
   PRIMARY KEY (name, year),
   CONSTRAINT fk_Course_Category
     FOREIGN KEY (Category_name )
@@ -86,7 +87,7 @@ CREATE  TABLE  Course (
   CONSTRAINT fk_Course_TeacherProfile
     FOREIGN KEY (TeacherProfile_User_email )
     REFERENCES TeacherProfile (User_email )
-    ON DELETE NO ACTION
+    ON DELETE SET NULL
     ON UPDATE NO ACTION);
 
 CREATE INDEX fk_Course_Category ON Course (Category_name ASC) ;
@@ -105,9 +106,9 @@ CREATE  TABLE  ForumThread (
   PRIMARY KEY (created_at, title) ,
   CONSTRAINT fk_ForumThread_Course
     FOREIGN KEY (Course_name , Course_year )
-    REFERENCES fn71100_71012.Course (name , year )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    REFERENCES Course (name , year )
+    ON DELETE CASCADE
+    );
 
 -- -----------------------------------------------------
 -- Table ForumReply
@@ -221,8 +222,8 @@ CREATE  TABLE  Rating (
   CONSTRAINT fk_Rating_TeacherProfile
     FOREIGN KEY (TeacherProfile_User_email )
     REFERENCES TeacherProfile (User_email )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON DELETE CASCADE 
+    );
 
 CREATE INDEX fk_Rating_Course ON Rating (Course_name ASC, Course_year ASC) ;
 
@@ -240,7 +241,7 @@ CREATE  TABLE  Assignment (
   max_points SMALLINT NOT NULL , --unsigned
   Course_name VARCHAR(255) NOT NULL ,
   Course_year SMALLINT NOT NULL ,
-  TeacherProfile_User_email VARCHAR(255) NOT NULL ,
+  TeacherProfile_User_email VARCHAR(255),
   created_at TIMESTAMP NOT NULL WITH DEFAULT,
   PRIMARY KEY (title, Course_name, Course_year) ,
   CONSTRAINT fk_Assignment_Course
@@ -250,8 +251,7 @@ CREATE  TABLE  Assignment (
   CONSTRAINT fk_Assignment_TeacherProfile
     FOREIGN KEY (TeacherProfile_User_email )
     REFERENCES TeacherProfile (User_email )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    );
 
 CREATE INDEX fk_Assignment_Course ON Assignment (Course_name ASC, Course_year ASC) ;
 
@@ -275,8 +275,8 @@ CREATE  TABLE  AssignmentGrade (
   CONSTRAINT fk_AssignmentGrade_Assignment
     FOREIGN KEY (Assignment_title , Assignment_Course_name , Assignment_Course_year )
     REFERENCES Assignment (title , Course_name , Course_year )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON DELETE CASCADE
+    );
 
 CREATE INDEX fk_AssignmentGrade_Assignment ON AssignmentGrade (Assignment_title ASC, Assignment_Course_name ASC, Assignment_Course_year ASC) ;
 
@@ -341,8 +341,8 @@ CREATE  TABLE  AssignmentFile (
   CONSTRAINT fk_AssignmentFile_Assignment
     FOREIGN KEY (Assignment_title , Assignment_Course_name , Assignment_Course_year )
     REFERENCES Assignment (title , Course_name , Course_year )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON DELETE CASCADE
+    );
 
 CREATE INDEX fk_AssignmentFile_Assignment ON AssignmentFile (Assignment_title ASC, Assignment_Course_name ASC, Assignment_Course_year ASC) ;
 
@@ -389,15 +389,15 @@ CREATE  TABLE  AssignmentNotification (
   body VARCHAR(6144) NOT NULL ,
   created_at TIMESTAMP NOT NULL WITH DEFAULT,
   id INTEGER GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
-  Assignment_title VARCHAR(255) NOT NULL ,
-  Assignment_Course_name VARCHAR(255) NOT NULL ,
-  Assignment_Course_year SMALLINT NOT NULL ,
-  PRIMARY KEY (id, Assignment_title, Assignment_Course_name, Assignment_Course_year) ,
+  Assignment_title VARCHAR(255),
+  Assignment_Course_name VARCHAR(255),
+  Assignment_Course_year SMALLINT,
+  PRIMARY KEY (id) ,
   CONSTRAINT fk_AssignmentNotification_Assignment
     FOREIGN KEY (Assignment_title , Assignment_Course_name , Assignment_Course_year )
     REFERENCES Assignment (title , Course_name , Course_year )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON DELETE SET NULL
+    );
 
 CREATE INDEX fk_AssignmentNotification_Assignment ON AssignmentNotification (Assignment_title ASC, Assignment_Course_name ASC, Assignment_Course_year ASC) ;
 
@@ -409,14 +409,14 @@ CREATE  TABLE  CourseNotification (
   body VARCHAR(6144) NOT NULL ,
   created_at TIMESTAMP NOT NULL WITH DEFAULT,
   id INTEGER GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
-  Course_name VARCHAR(255) NOT NULL ,
-  Course_year SMALLINT NOT NULL ,
+  Course_name VARCHAR(255) ,
+  Course_year SMALLINT ,
   PRIMARY KEY (id) ,
   CONSTRAINT fk_CourseNotification_Course
     FOREIGN KEY (Course_name , Course_year )
     REFERENCES Course (name , year )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON DELETE SET NULL
+  );
 
 CREATE INDEX fk_CourseNotification_Course ON CourseNotification (Course_name ASC, Course_year ASC) ;
 
@@ -427,15 +427,19 @@ CREATE  TABLE  ForumReplyNotification (
   body VARCHAR(1644) NOT NULL ,
   created_at TIMESTAMP NOT NULL WITH DEFAULT,
   id INTEGER GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
-  PRIMARY KEY (id) );
-
+  PRIMARY KEY (id),
+  ForumReply_created_at TIMESTAMP,
+  ForumReply_User_email VARCHAR(255),
+  CONSTRAINT fk_ForumReplyNotification_ForumReply
+    FOREIGN KEY (Course_name , Course_year )
+    REFERENCES Course (name , year )
+    ON DELETE SET NULL);
 -- -----------------------------------------------------
 -- Table Speciality
 -- -----------------------------------------------------
 CREATE  TABLE  Speciality (
   name VARCHAR(255) NOT NULL ,
   PRIMARY KEY (name) );
-
 
 -- -----------------------------------------------------
 -- Table SpecialityLookup
